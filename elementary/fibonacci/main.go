472c5c1d19dd242c961905760a29c32491852e41
fullstackly <48249303+fullstackly@users.noreply.github.com>
2019-07-02 21:16:10 +0300

    Add files via upload

--- elementary/fibonacci/main.go
+++ elementary/fibonacci/main.go
@@ -8,77 +8,104 @@ import (
 	"strings"
 )
 
-func validateArgs(args []string) (uint, uint, bool) {
+func validateArgs(args []string) (bottom, top int, err error) {
 
-	if len(args) == 2 {
-		if n, err := strconv.Atoi(args[0]); err == nil {
-			if m, err := strconv.Atoi(args[1]); err == nil {
-				if n > 0 && m > 0 && m > n {
-					return uint(n), uint(m), true
-				}
-			}
-		}
+	if len(args) != 2 {
+		err = fmt.Errorf("\nTwo integer numbers required, %d entered.\n", len(args))
+		return 0, 0, err
 	}
-	return 0, 0, false
+
+	bottom, err = strconv.Atoi(args[0])
+	if err != nil {
+		err = fmt.Errorf("\nFirst argument \"%v\" is not an integer number. \n%s\n", args[0], err)
+		return
+	}
+
+	top, err = strconv.Atoi(args[1])
+	if err != nil {
+		err = fmt.Errorf("\nSecond argument \"%v\" is not an integer number. \n%s\n", args[1], err)
+		return
+	}
+
+	if bottom < 0 || top < 0 {
+		err = fmt.Errorf("\nArguments have to be positive.\n")
+	} else if top < bottom {
+		err = fmt.Errorf("\nFirst argument %d have to be lower the second one %d.\n", bottom, top)
+	}
+
+	return
 }
 
-func getInput() (uint, uint, bool) {
-	for {
-		reader := bufio.NewReader(os.Stdin)
+func getInput(reader *bufio.Reader) (args []string, err error) {
+	fmt.Println("App returns slice of fibonacci sequence inbetween two boundary-numbers. \n" +
+		"Enter two not equal integer numbers: smaller first, then - larger one.")
 
-		fmt.Println("App returns slice of fibonacci sequence inbetween two boundary-numbers. ")
-		fmt.Println("Enter two not equal integer numbers: smaller first, then - larger one.")
-
-		if str, err := reader.ReadString('\n'); err == nil {
-			args := strings.Fields(str)
-			if n, m, isIt := validateArgs(args); isIt == true {
-				return n, m, true
-			}
+	str, err := reader.ReadString('\n')
+	if err == nil {
+		args = strings.Fields(str)
+		for i := range args {
+			strings.TrimSpace(args[i])
 		}
 	}
+	return
 }
 
-func fib(num int) uint {
+func fibonacci(num int) int {
 	if num <= 1 {
-		return uint(num)
+		return num
 	}
 
-	return fib(num-2) + fib(num-1)
+	return fibonacci(num-2) + fibonacci(num-1)
 }
 
-func fiBorders(x uint) (uint, uint) {
-	i := 0
-	j := 0
-	for fib(i) < x {
+func fibBorders(x int) (int, int) {
+	var i int
+
+	for fibonacci(i) < x {
 		i++
-	}
-	for fib(j) <= x {
-		j++
+		if fibonacci(i) == x {
+			return x, x
+		}
 	}
 
-	return fib(i), fib(j - 1)
+	return fibonacci(i - 1), fibonacci(i)
+}
+
+func fibSlice(bottom, top int) (fibSl []string) {
+	for i := 0; fibonacci(i) <= top; i++ {
+		if fibonacci(i) >= bottom {
+			fibSl = append(fibSl, strconv.Itoa(int(fibonacci(i))))
+		}
+	}
+	return
 }
 
 func main() {
-	var p, q uint
-	var fibSl []string
+	var bottom, top int
+	var err = fmt.Errorf("No inputs received.")
 
-	if n, m, ok := validateArgs(os.Args[1:]); ok == true {
-		p, _ = fiBorders(n)
-		_, q = fiBorders(m)
-	} else if n, m, ok := getInput(); ok == true {
-		p, _ = fiBorders(n)
-		_, q = fiBorders(m)
+	if len(os.Args) > 2 {
+		bottom, top, err = validateArgs(os.Args[1:])
 	}
 
-	for i := 0; fib(i) <= q; i++ {
-		if fib(i) >= p {
-			fibSl = append(fibSl, strconv.Itoa(int(fib(i))))
+	for err != nil {
+		fmt.Println(err)
+		var inputs []string
+		inputs, err = getInput(bufio.NewReader(os.Stdin))
+		if err == nil {
+			bottom, top, err = validateArgs(inputs)
 		}
 	}
+
+	_, p := fibBorders(bottom)
+	q, _ := fibBorders(top)
+
+	fibSl := fibSlice(p, q)
+
 	if len(fibSl) > 0 {
 		fmt.Println(strings.Join(fibSl, ", "))
 	} else {
-		fmt.Println("No fibonacci numbers inbetween given boundaries.")
+		fmt.Println("No fibonacci numbers between given boundaries.")
 	}
+
 }
